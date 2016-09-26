989c634c20b0318cabe6456c4c64b66f1c0fa4f5
Songlj11 <rich@richdemacbook-air.local>
2016-09-26 10:12:34 +0800

    First submit

---
+++ Test/Test/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Test
+//
+//  Created by rich on 16/9/26.
+//  Copyright © 2016年 Song. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
